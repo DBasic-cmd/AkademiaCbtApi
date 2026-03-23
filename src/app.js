1b32578135687910d0c8ac72c402248993ad75ee
Babatunji <oekisola589@gmail.com>
2026-03-23 13:07:54 +0100

    minor intended fix

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 
-const app = express();
+
 // Add this near the top of app.js if not already there
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
+const app = express();
 
 // Place this BEFORE any routes
 app.use(cors({
-  origin: [ 'http://localhost:8080/','https://akademiacbtapi.onrender.com'], 
+  origin: [ 'http://localhost:8080','https://akademia-cbt.vercel.app'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Required if you are using cookies or sessions
